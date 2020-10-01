78a276a8d3527ea368de4aa107cdeca19822d32a
Ana Gadelho <up201806309@fe.up.pt>
2020-10-01 23:24:58 +0100

    added torus

--- TP1/main.js
+++ TP1/main.js
@@ -11,7 +11,7 @@ function getUrlVars() {
     return vars;
 }	 
 //Include additional files here
-serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'MyRectangle.js','Material.js','Stack.js','MySphere.js','MyCylinder.js',
+serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'MyRectangle.js','Material.js','Stack.js','MySphere.js','MyCylinder.js','MyTorus.js',
 
 main=function()
 {
